70ce32378a79cec34e19b5b6afdad413dc8abd20
Crystal <crystalwatkins428@gmail.com>
2020-06-04 21:19:19 -0400

    updated prompts to work

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,25 +2,19 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-  function writePassword() {
-    var lengthOfPassword = prompt("Enter Your Desired Length of Password.")
-  if(lengthOfPassword < 8 || lengthOfPassword > 128 {
-    alert("Enter a Number That is Between 8 and 128.");
-    return;
-  } if ("number" != typeof(lengthOfPassword)) {
-    alert("Please enter a valid number.");
-  }
-  
+function writePassword() {
+  var lengthOfPassword = prompt("Please enter desired password's length.")
 
+if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+alert("Please enter a Number between 8 and 128")
+} else if (isNaN(lengthOfPassword)) {
+  alert("Please enter a valid number.");
+}
 
-
-
-  var password = generatePassword();
+var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
