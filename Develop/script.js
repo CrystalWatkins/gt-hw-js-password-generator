ecd14a8b84d4eac9c55b59c627819d199e2ebd3a
Crystal <crystalwatkins428@gmail.com>
2020-06-05 13:17:48 -0400

    continued work

--- Develop/script.js
+++ Develop/script.js
@@ -19,47 +19,44 @@ function writePassword() {
   var numbers = confirm("Do you want Numbers- Click OK or Cancel");
   var specialCharacters = confirm("Do you want Special Characters- Click OK or Cancel");
 
-  var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var specialCharacters = ["!", "@", "#", "$", "%", "^", "*", "_", "+", "~", "|", "?","-", "=", "?"];
+  var upperCaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerCaseLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
+  var numbersArray = [0,1,2,3,4,5,6,7,8,9];
+  var specialCharactersArray = ["!", "@", "#", "$", "%", "^", "*", "_", "+", "~", "|", "?","-", "=", "?"];
 
   var userOption = [];
 
-  if (upperCaseLetters) {
-    userOption = userOption.concat(upperCaseLetters);
+  if (upperCaseLetters === true) {
+    userOption = userOption.concat(upperCaseLettersArray);
   }
-
-  if (lowerCaseLetters) {
-    userOption = userOption.concat(lowerCaseLetters);
+  if (lowerCaseLetters === true) {
+    userOption = userOption.concat(lowerCaseLettersArray);
   }
-
-  if (numbers) {
-    userOption = userOption.concat(numbers);
+  if (numbers ===true) {
+    userOption = userOption.concat(numbersArray);
   }
-
-  if (specialCharacters) {
-    userOption = userOption.concat(specialCharacters);
+  if (specialCharacters ===true) {
+    userOption = userOption.concat(specialCharactersArray);
   }
-
-  if (userOption.length === 0) {
-    alert("please select at least one character type");
-    return lengthOfPassword();
+  else if (upperCaseLetters === false && lowerCaseLetters === false && numbers === false && specialCharacters === false) {
+    alert("Please select at least one character type");
+    return writePassword();
   }
-
   console.log(userOption);
 
-  var password = "";
-  
-  for (var i = 0; i < lengthOfPassword; i++) {
+  for (var i=0; i  < lengthOfPassword; i++) {
     var ranNum = Math.floor(Math.random() * userOption.length);
-    console.log(ranNum);
-
-    password += userOption[ranNum];
-    console.log(password);
-
-    console.log(userOption[ranNum]);
   }
+
+  // var password = "";
+
+    // console.log(ranNum);
+
+    // password += userOption[ranNum];
+    // console.log(password);
+
+    // console.log(userOption[ranNum]);
+  // }
 
 var password = generatePassword();
 var passwordText = document.querySelector("#password");
