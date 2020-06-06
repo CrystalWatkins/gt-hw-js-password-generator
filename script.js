0fe9f02777baacdf0592225637440adc82cf0519
Crystal <crystalwatkins428@gmail.com>
2020-06-05 21:34:59 -0400

    added picture to read me and notes to js page

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var generateBtn = document.querySelector("#generate");
 alert("Please click Generate Password to Begin")
 function writePassword() {
   var lengthOfPassword = prompt("Please enter desired password's length.")
-  
+  //The above line sets up how long the password will be
   if (lengthOfPassword < 8 || lengthOfPassword > 128) {
     alert("Please enter a Number between 8 and 128")
     return writePassword();
@@ -14,18 +14,24 @@ function writePassword() {
     return writePassword();
   }
   console.log(lengthOfPassword);
+  //the return code will allow the page to continue to go back until
+  //it has received a valid input (ie number). The else if statement insures that
+  //the input received is a number and the first if statement makes sure it is 
+  // between 8 and 128 characters
 
   var upperCaseLetters = confirm("Do you want Upper Case Letters- Click OK or Cancel");
   var lowerCaseLetters = confirm("Do you want Lower Case Letters- Click OK or Cancel");
   var numbers = confirm("Do you want Numbers- Click OK or Cancel");
   var specialCharacters = confirm("Do you want Special Characters- Click OK or Cancel");
-
+  //This asks the user what letters, numbers, or special characters that they want
+  // in their password
+  
   var upperCaseLettersArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerCaseLettersArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k","l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; 
   var numbersArray = [0,1,2,3,4,5,6,7,8,9];
   var specialCharactersArray = ["!", "@", "#", "$", "%", "^", "*", "_", "+", "~", "|", "?","-", "=", "?"];
-
   var userOption = [];
+  //this is declaring the string of things that can be used in the password
 
   if (upperCaseLetters === true) {
     userOption = userOption.concat(upperCaseLettersArray);
@@ -44,6 +50,8 @@ function writePassword() {
     return writePassword();
   }
   console.log(userOption);
+  //these if statements are used to pull from the arrays declared above if they are selected. In the console.log
+  //you can see what variables there are to choose from for the password generator
 
   var password= "";
   for (var i=0; i  < lengthOfPassword; i++) {
@@ -51,10 +59,14 @@ function writePassword() {
     console.log(ranNum);
     password += userOption[ranNum];
   }
-  
+
 console.log(password);
+// The loop ran above states that you are going to randomly generate one item from the arrays chosen
+// until the length of the password is met. It is also console.log(ed) at the bottom of the page
 
 // var password = generatePassword(); 
+// For the password to generate in the box, I commented out the above line of code. It will still
+// show up in the console.log either way. Not sure why this happened
 var passwordText = document.querySelector("#password");
 
 passwordText.value = password;
